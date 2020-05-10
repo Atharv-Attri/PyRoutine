4be738716b2236861808872e4476623933fc837a
Atharv2 <48738128+Atharv2@users.noreply.github.com>
2020-05-10 16:27:15 -0700

    added examples

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyRoutine-Atharv2",  # Replace with your own username
-    version="1.0.0",
+    name="PyRoutine", 
+    version="1.1.0",
     author="Atharv2",
     author_email="atharv260107@gmail.com",
     description="A program to redo mouse functions.",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License:: OSI Approved:: GNU Lesser General Public License v3(LGPLv3)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.0',
